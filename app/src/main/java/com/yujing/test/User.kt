83a7b85c1079150797549defe1c53ed3e6aec413
yu1441 <yu1441@qq.com>
2020-04-02 09:40:21 +0800

    AS更新到3.6.2

--- app/src/main/java/com/yujing/test/User.kt
+++ app/src/main/java/com/yujing/test/User.kt
@@ -1,9 +1,5 @@
 package com.yujing.test
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-import java.io.Serializable
-
 data class User (
     var name: String,
     var sex: Int
