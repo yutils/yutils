1ac94b77340e528d95877698dafb39304945024d
yu1441 <yu1441@qq.com>
2020-04-29 14:47:45 +0800

    YLog 加上 public 修饰 打印日志能打印更多

--- utils/src/main/java/com/yujing/utils/YLog.java
+++ utils/src/main/java/com/yujing/utils/YLog.java
@@ -2,10 +2,7 @@ package com.yujing.utils;
 
 import android.util.Log;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import java.util.List;
 
 /**
  * LOG显示类，解决AndroidStudio的logcat显示超长字符串的问题
@@ -16,7 +13,7 @@ import com.google.gson.JsonParser;
 @SuppressWarnings({"unused", "FieldCanBeLocal", "WeakerAccess"})
 public class YLog {
     //规定每段显示的长度
-    private static int LOG_MAX_LENGTH = 2000;
+    private static int LOG_MAX_LENGTH = 4000;
     private static String TAG = "YLog";
     private static final int VERBOSE = 2;
     private static final int DEBUG = 3;
@@ -89,7 +86,7 @@ public class YLog {
     }
 
     public static void json(String TAG, String str) {
-        d(TAG,YUtils.jsonFormat(str));
+        d(TAG, YUtils.jsonFormat(str));
     }
 
     /**
@@ -101,39 +98,67 @@ public class YLog {
      * @param type 类型
      */
     private static void println(String TAG, String msg, Throwable tr, int type) {
-        int strLength = msg.length();
-        int start = 0;
-        int end = LOG_MAX_LENGTH;
-        for (int i = 0; i < 100; i++) {
-            //剩下的文本还是大于规定长度则继续重复截取并输出
-            if (strLength > end) {
-                String tag = TAG + i;
-                if (type == VERBOSE)
-                    Log.v(tag, msg.substring(start, end), tr);
-                else if (type == DEBUG)
-                    Log.d(tag, msg.substring(start, end), tr);
-                else if (type == INFO)
-                    Log.i(tag, msg.substring(start, end), tr);
-                else if (type == WARN)
-                    Log.w(tag, msg.substring(start, end), tr);
-                else if (type == ERROR)
-                    Log.e(tag, msg.substring(start, end), tr);
-                start = end;
-                end = end + LOG_MAX_LENGTH;
-            } else {
-                String tag = i == 0 ? TAG : TAG + i;
-                if (type == VERBOSE) {
-                    Log.v(tag, msg.substring(start, strLength), tr);
-                } else if (type == DEBUG)
-                    Log.d(tag, msg.substring(start, strLength), tr);
-                else if (type == INFO)
-                    Log.i(tag, msg.substring(start, strLength), tr);
-                else if (type == WARN)
-                    Log.w(tag, msg.substring(start, strLength), tr);
-                else if (type == ERROR)
-                    Log.e(tag, msg.substring(start, strLength), tr);
-                break;
-            }
+        List<StringBuilder> lines = YString.groupActual(msg, LOG_MAX_LENGTH);
+        int i = 1;
+        for (StringBuilder item : lines) {
+            String tag = lines.size() == 1 ? TAG : TAG + i;
+            if (type == VERBOSE)
+                Log.v(tag, item.toString(), tr);
+            else if (type == DEBUG)
+                Log.d(tag, item.toString(), tr);
+            else if (type == INFO)
+                Log.i(tag, item.toString(), tr);
+            else if (type == WARN)
+                Log.w(tag, item.toString(), tr);
+            else if (type == ERROR)
+                Log.e(tag, item.toString(), tr);
+            i++;
         }
     }
+
+//    /**
+//     * 打印日志
+//     *
+//     * @param TAG  tag
+//     * @param msg  内容
+//     * @param tr   异常
+//     * @param type 类型
+//     */
+//    @Deprecated
+//    private static void println(String TAG, String msg, Throwable tr, int type) {
+//        int strLength = msg.length();
+//        int start = 0;
+//        int end = LOG_MAX_LENGTH;
+//        for (int i = 0; i < 100; i++) {
+//            //剩下的文本还是大于规定长度则继续重复截取并输出
+//            if (strLength > end) {
+//                String tag = TAG + i;
+//                if (type == VERBOSE)
+//                    Log.v(tag, msg.substring(start, end), tr);
+//                else if (type == DEBUG)
+//                    Log.d(tag, msg.substring(start, end), tr);
+//                else if (type == INFO)
+//                    Log.i(tag, msg.substring(start, end), tr);
+//                else if (type == WARN)
+//                    Log.w(tag, msg.substring(start, end), tr);
+//                else if (type == ERROR)
+//                    Log.e(tag, msg.substring(start, end), tr);
+//                start = end;
+//                end = end + LOG_MAX_LENGTH;
+//            } else {
+//                String tag = i == 0 ? TAG : TAG + i;
+//                if (type == VERBOSE) {
+//                    Log.v(tag, msg.substring(start, strLength), tr);
+//                } else if (type == DEBUG)
+//                    Log.d(tag, msg.substring(start, strLength), tr);
+//                else if (type == INFO)
+//                    Log.i(tag, msg.substring(start, strLength), tr);
+//                else if (type == WARN)
+//                    Log.w(tag, msg.substring(start, strLength), tr);
+//                else if (type == ERROR)
+//                    Log.e(tag, msg.substring(start, strLength), tr);
+//                break;
+//            }
+//        }
+//    }
 }
--- utils/src/main/java/com/yujing/utils/YString.java
+++ utils/src/main/java/com/yujing/utils/YString.java
@@ -1,10 +1,12 @@
 package com.yujing.utils;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 对String的一些处理方法
+ *
  * @author yujing  2019年4月2日10:27:01
  */
 @SuppressWarnings({"unused", "WeakerAccess"})
@@ -44,6 +46,7 @@ public class YString {
         }
         return new String(c);
     }
+
     /**
      * 字符串分组，每digit位字符拆分一次字符串，中文英文都算一个字符
      *
@@ -67,6 +70,7 @@ public class YString {
         return strings;
     }
 
+
     /**
      * 字符串分组，每digit位字符拆分一次字符串，英文算一个字符，中文算两个字符
      *
@@ -80,12 +84,69 @@ public class YString {
         int index = 0;
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);//获取每一个字
+            index = (c >= 1 && c <= 127) ? index + 1 : index + 2;
+            if (index > digit) {//如果大于就换行
+                index = 0;
+                strings.add(sb);
+                sb = new StringBuilder();
+                i--;
+                continue;
+            }
             sb.append(c);
-            if (c >= 1 && c <= 127)
-                index++;
-            else
-                index += 2;
             if (index >= digit) {//如果大于2倍就换行
+                index = 0;
+                strings.add(sb);
+                sb = new StringBuilder();
+            }
+        }
+        if (sb.length() > 0)
+            strings.add(sb);
+        return strings;
+    }
+
+    /**
+     * 字符串分组，真实长度，每digit位字符拆分一次字符串，英文算一个字符，中文算两个或者3字符
+     *
+     * @param str   字符串
+     * @param digit 位
+     * @return 拆分后的字符串
+     */
+    public static List<StringBuilder> groupActual(String str, int digit) {
+        return groupActual(str, digit, null);
+    }
+
+    /**
+     * 字符串分组，指定字符串编码
+     *
+     * @param str     字符串
+     * @param digit   位
+     * @param charset 编码
+     * @return 拆分后的字符串
+     */
+    public static List<StringBuilder> groupActual(String str, int digit, Charset charset) {
+        if (digit < 3) return group(str, digit);
+        List<StringBuilder> strings = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            char c = str.charAt(i);//获取每一个字
+            if (charset != null)
+                index += String.valueOf(c).getBytes(charset).length;
+            else
+                index += String.valueOf(c).getBytes().length;
+
+            //如果大于就换行
+            if (index > digit) {
+                index = 0;
+                strings.add(sb);
+                sb = new StringBuilder();
+                i--;
+                continue;
+            }
+            //连接字符串
+            sb.append(c);
+            if (index == digit) {//如果大于就换行
                 index = 0;
                 strings.add(sb);
                 sb = new StringBuilder();
