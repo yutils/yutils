4bce996c0a60f637700f2d56b4a90a7d9230d4b1
yu1441 <yu1441@qq.com>
2020-07-27 10:37:19 +0800

    新增kotlin语言，新增YMessage类

---
+++ utils/src/main/java/com/yujing/contract/YMessage.kt
@@ -0,0 +1,30 @@
+package com.yujing.contract
+
+import java.io.Serializable
+
+
+/**
+ * 消息类，发送或者接收消息
+ * @author yujing 2020年7月27日09:50:30
+ */
+open class YMessage<T>(private var type: String?, private val data: T) : Serializable {
+    override fun toString(): String {
+        //如果引用的有Gson包就用Gson序列化
+        try {
+            val cls = Class.forName("com.google.gson.Gson")
+            val obj = cls.newInstance()
+            val method = cls.getMethod("toJson", Any::class.java)
+            val dataJson = method.invoke(obj, data) as String
+            return "RxBusMessage{" +
+                    "type='" + type + '\'' +
+                    ", data=" + dataJson +
+                    '}'
+        } catch (e: Exception) {
+            //如果失败就用Yjson转json
+            return "RxBusMessage{" +
+                    "type='" + type + '\'' +
+                    ", data=" + data +
+                    '}'
+        }
+    }
+}
