f72a64054927308bf4557022dd3a444025581f0d
yu1441 <yu1441@qq.com>
2020-09-03 16:32:33 +0800

    更新RSA

--- utils/src/main/java/com/yujing/crypt/YRsa.java
+++ utils/src/main/java/com/yujing/crypt/YRsa.java
@@ -21,6 +21,7 @@ import javax.crypto.Cipher;
 
 /**
  * RSA加密解密
+ *
  * @author 余静 2020年9月3日14:11:14
  */
 /*用法
@@ -61,146 +62,188 @@ boolean result =  YRsa.verify(content.getBytes(), publicKeyString, sign);
 System.out.println("验签结果：" + result);
  */
 public class YRsa {
-	private final static String RSA_ALGORITHM = "RSA";
-	private final static String SIGNATURE_ALGORITHM = "MD5withRSA";
-	private final static String RSA_PUBLIC_KEY = "RSAPublicKey";
-	private final static String RSA_PRIVATE_KEY = "RSAPrivateKey";
+    //加密解密方式
+    private final static String RSA_ALGORITHM = "RSA";
+    //签名算法
+    private final static String SIGNATURE_ALGORITHM = "MD5withRSA";
+    //公钥 map ----> key
+    private final static String RSA_PUBLIC_KEY = "RSAPublicKey";
+    //私钥 map ----> key
+    private final static String RSA_PRIVATE_KEY = "RSAPrivateKey";
+    //填充方式
+    public static String RSA_PADDING_STYLE = "RSA/ECB/PKCS1Padding";
 
-	/**
-	 * 使用私钥对数据进行加密
-	 */
-	public static byte[] encryptPrivateKey(byte[] binaryData, String privateKey) throws Exception {
-		byte[] keyBytes = YBase64.decode(privateKey);
-		PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
-		// 获取RSA算法实例
-		KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
-		Key priKey = keyFactory.generatePrivate(keySpec);
-		// 初始化加密器
-		//Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
-		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-		cipher.init(Cipher.ENCRYPT_MODE, priKey);
-		return cipher.doFinal(binaryData);
-	}
+    /**
+     * 使用私钥对数据进行加密
+     *
+     * @param binaryData 要加密的数据
+     * @param privateKey 私钥Base64
+     * @return 加密后的数据
+     * @throws Exception Exception
+     */
+    public static byte[] encryptPrivateKey(byte[] binaryData, String privateKey) throws Exception {
+        byte[] keyBytes = YBase64.decode(privateKey);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        // 获取RSA算法实例
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
+        Key priKey = keyFactory.generatePrivate(keySpec);
+        // 初始化加密器
+        //Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        Cipher cipher = Cipher.getInstance(RSA_PADDING_STYLE);
+        cipher.init(Cipher.ENCRYPT_MODE, priKey);
+        return cipher.doFinal(binaryData);
+    }
 
-	/**
-	 * 使用公钥对数据进行加密
-	 */
-	public static byte[] encryptPublicKey(byte[] binaryData, String publicKey) throws Exception {
-		byte[] keyBytes = YBase64.decode(publicKey);
-		X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
-		// 获取RSA算法实例
-		KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
-		Key pubKey = keyFactory.generatePublic(keySpec);
-		// 初始化加密器
-		//Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
-		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-		cipher.init(Cipher.ENCRYPT_MODE, pubKey);
-		return cipher.doFinal(binaryData);
-	}
+    /**
+     * 使用公钥对数据进行加密
+     *
+     * @param binaryData 要加密的数据
+     * @param publicKey  公钥Base64
+     * @return 加密后的数据
+     * @throws Exception Exception
+     */
+    public static byte[] encryptPublicKey(byte[] binaryData, String publicKey) throws Exception {
+        byte[] keyBytes = YBase64.decode(publicKey);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        // 获取RSA算法实例
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
+        Key pubKey = keyFactory.generatePublic(keySpec);
+        // 初始化加密器
+        //Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        Cipher cipher = Cipher.getInstance(RSA_PADDING_STYLE);
+        cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+        return cipher.doFinal(binaryData);
+    }
 
-	/**
-	 * 使用私钥对数据进行解密
-	 */
-	public static byte[] decryptPrivateKey(byte[] binaryData, String privateKey) throws Exception {
-		byte[] keyBytes = YBase64.decode(privateKey);
-		PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
-		// 获取RSA算法实例
-		KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
-		Key priKey = keyFactory.generatePrivate(keySpec);
-		// 初始化加密器
-		//Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
-		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-		cipher.init(Cipher.DECRYPT_MODE, priKey);
-		return cipher.doFinal(binaryData);
-	}
+    /**
+     * 使用私钥对数据进行解密
+     *
+     * @param binaryData 要解密的数据
+     * @param privateKey 私钥Base64
+     * @return 解密后的数据
+     * @throws Exception Exception
+     */
+    public static byte[] decryptPrivateKey(byte[] binaryData, String privateKey) throws Exception {
+        byte[] keyBytes = YBase64.decode(privateKey);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        // 获取RSA算法实例
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
+        Key priKey = keyFactory.generatePrivate(keySpec);
+        // 初始化加密器
+        //Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        Cipher cipher = Cipher.getInstance(RSA_PADDING_STYLE);
+        cipher.init(Cipher.DECRYPT_MODE, priKey);
+        return cipher.doFinal(binaryData);
+    }
 
-	/**
-	 * 使用公钥对数据进行解密
-	 */
-	public static byte[] decryptPublicKey(byte[] binaryData, String publicKey) throws Exception {
-		byte[] keyBytes = YBase64.decode(publicKey);
-		X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(keyBytes);
-		// 获取RSA算法实例
-		KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
-		Key pubKey = keyFactory.generatePublic(x509KeySpec);
-		// 初始化加密器
-		//Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
-		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-		cipher.init(Cipher.DECRYPT_MODE, pubKey);
-		return cipher.doFinal(binaryData);
-	}
+    /**
+     * 使用公钥对数据进行解密
+     *
+     * @param binaryData 要解密的数据
+     * @param publicKey  公钥Base64
+     * @return 解密后的数据
+     * @throws Exception Exception
+     */
+    public static byte[] decryptPublicKey(byte[] binaryData, String publicKey) throws Exception {
+        byte[] keyBytes = YBase64.decode(publicKey);
+        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(keyBytes);
+        // 获取RSA算法实例
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
+        Key pubKey = keyFactory.generatePublic(x509KeySpec);
+        // 初始化加密器
+        //Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        Cipher cipher = Cipher.getInstance(RSA_PADDING_STYLE);
+        cipher.init(Cipher.DECRYPT_MODE, pubKey);
+        return cipher.doFinal(binaryData);
+    }
 
-	/**
-	 * 使用私钥对数据进行签名
-	 */
-	public static String sign(byte[] binaryData, String privateKey)
-			throws Exception {
-		byte[] keyBytes = YBase64.decode(privateKey);
-		PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
-		// 获取RSA算法实例
-		KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
-		PrivateKey priKey = keyFactory.generatePrivate(keySpec);
-		// 获取签名算法
-		Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
-		signature.initSign(priKey);
-		signature.update(binaryData);
-		return YBase64.encode(signature.sign());
-	}
+    /**
+     * 使用私钥对数据进行签名
+     *
+     * @param binaryData 要签名的数据
+     * @param privateKey 私钥Base64
+     * @return 签名后的数据 Base64
+     * @throws Exception Exception
+     */
+    public static String sign(byte[] binaryData, String privateKey)
+            throws Exception {
+        byte[] keyBytes = YBase64.decode(privateKey);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        // 获取RSA算法实例
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
+        PrivateKey priKey = keyFactory.generatePrivate(keySpec);
+        // 获取签名算法
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+        signature.initSign(priKey);
+        signature.update(binaryData);
+        return YBase64.encode(signature.sign());
+    }
 
-	/**
-	 * 使用公钥对数据签名进行验证
-	 */
-	public static boolean verify(byte[] binaryData, String publicKey, String sign) throws Exception {
-		byte[] keyBytes = YBase64.decode(publicKey);
-		X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
-		// 获取RSA算法实例
-		KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
-		PublicKey pubKey = keyFactory.generatePublic(keySpec);
-		// 获取签名算法
-		Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
-		signature.initVerify(pubKey);
-		signature.update(binaryData);
-		return signature.verify(YBase64.decode(sign));
-	}
+    /**
+     * 使用公钥对数据签名进行验证
+     *
+     * @param binaryData 要验证的数据
+     * @param publicKey  公钥Base64
+     * @param sign       签名Base64
+     * @return 是否验证通过
+     * @throws Exception Exception
+     */
+    public static boolean verify(byte[] binaryData, String publicKey, String sign) throws Exception {
+        byte[] keyBytes = YBase64.decode(publicKey);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        // 获取RSA算法实例
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
+        PublicKey pubKey = keyFactory.generatePublic(keySpec);
+        // 获取签名算法
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+        signature.initVerify(pubKey);
+        signature.update(binaryData);
+        return signature.verify(YBase64.decode(sign));
+    }
 
-	/**
-	 * 获取一对公钥和私钥
-	 * @return  Map，RSA_PUBLIC_KEY，RSA_PRIVATE_KEY
-	 * @throws NoSuchAlgorithmException NoSuchAlgorithmException
-	 */
-	public static Map<String, Object> getKey() throws NoSuchAlgorithmException {
-		// 因为只存公钥和私钥，所以指明Map的长度是2
-		Map<String, Object> keyMap = new HashMap<>(2);
-		// 获取RSA算法实例
-		KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(RSA_ALGORITHM);
-		// 1024代表密钥二进制位数
-		keyPairGen.initialize(1024);
-		// 产生KeyPair工厂
-		KeyPair keyPair = keyPairGen.generateKeyPair();
-		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-		keyMap.put(RSA_PUBLIC_KEY, publicKey);
-		keyMap.put(RSA_PRIVATE_KEY, privateKey);
-		return keyMap;
-	}
+    /**
+     * 获取一对公钥和私钥
+     *
+     * @return Map，RSA_PUBLIC_KEY，RSA_PRIVATE_KEY
+     * @throws NoSuchAlgorithmException NoSuchAlgorithmException
+     */
+    public static Map<String, Object> getKey() throws NoSuchAlgorithmException {
+        // 因为只存公钥和私钥，所以指明Map的长度是2
+        Map<String, Object> keyMap = new HashMap<>(2);
+        // 获取RSA算法实例
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(RSA_ALGORITHM);
+        // 1024代表密钥二进制位数
+        keyPairGen.initialize(1024);
+        // 产生KeyPair工厂
+        KeyPair keyPair = keyPairGen.generateKeyPair();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        keyMap.put(RSA_PUBLIC_KEY, publicKey);
+        keyMap.put(RSA_PRIVATE_KEY, privateKey);
+        return keyMap;
+    }
 
-	/**
-	 * 取出map中的公钥
-	 * @param map 一对公钥和私钥
-	 * @return base64
-	 */
-	public static String getPublicKey(Map<String, Object> map) {
-		RSAPublicKey publicKey = (RSAPublicKey) map.get(RSA_PUBLIC_KEY);
-		return YBase64.encode(publicKey.getEncoded());
-	}
+    /**
+     * 取出map中的公钥
+     *
+     * @param map 一对公钥和私钥
+     * @return base64
+     */
+    public static String getPublicKey(Map<String, Object> map) {
+        RSAPublicKey publicKey = (RSAPublicKey) map.get(RSA_PUBLIC_KEY);
+        if (publicKey == null) return null;
+        return YBase64.encode(publicKey.getEncoded());
+    }
 
-	/**
-	 * 取出map中的私钥
-	 * @param map 一对公钥和私钥
-	 * @return base64
-	 */
-	public static String getPrivateKey(Map<String, Object> map) {
-		RSAPrivateKey privateKey = (RSAPrivateKey) map.get(RSA_PRIVATE_KEY);
-		return YBase64.encode(privateKey.getEncoded());
-	}
+    /**
+     * 取出map中的私钥
+     *
+     * @param map 一对公钥和私钥
+     * @return base64
+     */
+    public static String getPrivateKey(Map<String, Object> map) {
+        RSAPrivateKey privateKey = (RSAPrivateKey) map.get(RSA_PRIVATE_KEY);
+        if (privateKey == null) return null;
+        return YBase64.encode(privateKey.getEncoded());
+    }
 }
