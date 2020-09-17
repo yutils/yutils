9dc808e25db1baa975e6777150772afb7ae6cca3
yu1441 <yu1441@qq.com>
2020-09-17 19:08:10 +0800

    更新AES

--- utils/src/main/java/com/yujing/crypt/YAes.java
+++ utils/src/main/java/com/yujing/crypt/YAes.java
@@ -3,165 +3,187 @@ package com.yujing.crypt;
 import com.yujing.utils.YBase64;
 import com.yujing.utils.YConvert;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
+
 /**
- * Aes256 加密解密
- *
- * @author yujing 2019年8月27日16:28:24
+ * Aes128 加密解密
+ * @author yujing 2020年9月17日18:59:38
  */
 @SuppressWarnings("unused")
 public class YAes {
-    private static final String TRANSFORMATION = "AES/CBC/NoPadding";
-    //16位byte[]
-
-    public static SecretKeySpec getKey() {
-        byte[] KEY = {8, 5, 1, 6, 1, 4, 4, 1, 1, 9, 9, 0, 0, 5, 2, 5};
-        return  new SecretKeySpec(KEY, "AES");
+    private static final String AES = "AES";
+    private static final String AES_CBC_Padding = "AES/CBC/PKCS5Padding";
+    private static final String AES_ECB_Padding = "AES/ECB/PKCS5Padding";
+    /**
+     * 创建一个随机秘钥
+     *
+     * @return 秘钥
+     */
+    public static SecretKeySpec createKey() throws Exception {
+        // 生成key,构造密钥生成器，指定为AES算法,不区分大小写
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
+        //生成一个128位的随机源,根据传入的字节数组
+        keyGenerator.init(128);
+        //产生原始对称密钥
+        SecretKey secretKey = keyGenerator.generateKey();
+        //获得原始对称密钥的字节数组
+        byte[] keyBytes = secretKey.getEncoded();
+        // key转换,根据字节数组生成AES密钥
+        return new SecretKeySpec(keyBytes, AES);
     }
 
     /**
      * 创建一个随机秘钥
+     *
+     * @param key 根据password创建
      * @return 秘钥
      */
-    public static Key createKey() {
-        try {
-            // 生成key
-            KeyGenerator keyGenerator;
-            //构造密钥生成器，指定为AES算法,不区分大小写
-            keyGenerator = KeyGenerator.getInstance("AES");
-            //生成一个128位的随机源,根据传入的字节数组
-            keyGenerator.init(128);
-            //产生原始对称密钥
-            SecretKey secretKey = keyGenerator.generateKey();
-            //获得原始对称密钥的字节数组
-            byte[] keyBytes = secretKey.getEncoded();
-            // key转换,根据字节数组生成AES密钥
-            Key key = new SecretKeySpec(keyBytes, "AES");
-            return key;
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    /**加密
-     * @param context 需要加密的明文
-     * @param key 加密用密钥
-     * @return 加密后的内容
-     */
-    public static byte[] encrypt(String context, Key key) {
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            //将加密并编码后的内容解码成字节数组
-            return cipher.doFinal(context.getBytes());
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-
-    /** 解密
-     * @param result 加密后的密文byte数组
-     * @param key 解密用密钥
-     * @return 解密后的内容
-     */
-    public static byte[] decrypt(byte[] result, Key key) {
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            //初始化密码器，第一个参数为加密(Encrypt_mode)或者解密(Decrypt_mode)操作，第二个参数为使用的KEY
-            cipher.init(Cipher.DECRYPT_MODE, key);
-            return cipher.doFinal(result);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    public static SecretKeySpec createKey(byte[] key) throws Exception {
+        // 生成key//构造密钥生成器，指定为AES算法,不区分大小写
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
+        //生成一个128位的随机源,根据传入的字节数组
+        keyGenerator.init(128, new SecureRandom(key));
+        //产生原始对称密钥
+        SecretKey secretKey = keyGenerator.generateKey();
+        //获得原始对称密钥的字节数组
+        byte[] keyBytes = secretKey.getEncoded();
+        // key转换,根据字节数组生成AES密钥
+        return new SecretKeySpec(keyBytes, AES);
     }
 
     /**
      * 加密
      *
-     * @param bytes    加密对象
-     * @param password 密码
-     * @return 结果
+     * @param context 需要加密的明文
+     * @param key     加密用密钥
+     * @return 加密后的内容
      */
-    public static byte[] encrypt(byte[] bytes, String password) {
-        try {
-            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-            byte[] iv = createIV(password);
-            IvParameterSpec ipc= new IvParameterSpec(iv);
-            cipher.init(Cipher.ENCRYPT_MODE, getKey(),ipc);
-            return cipher.doFinal(paddingData(bytes));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+    public static byte[] encrypt(byte[] context, Key key) throws Exception {
+        Cipher cipher = Cipher.getInstance(AES_ECB_Padding);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        //将加密并编码后的内容解码成字节数组
+        return cipher.doFinal(context);
     }
 
     /**
      * 解密
      *
-     * @param bytes    解密对象
-     * @param password 密码
-     * @return 结果
+     * @param result 加密后的密文byte数组
+     * @param key    解密用密钥
+     * @return 解密后的内容
      */
-    public static byte[] decrypt(byte[] bytes, String password) {
-        try {
-            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-            byte[] iv = createIV(password);
-            IvParameterSpec ipc= new IvParameterSpec(iv);
-            cipher.init(Cipher.DECRYPT_MODE, getKey(), ipc);
-            byte[] decy = cipher.doFinal(bytes);
-            //去除decy末尾的0
-            int i = decy.length - 1;
-            for (; i >= 0; i--) {
-                if (decy[i] != 0) {
-                    break;
-                }
-            }
-            byte[] nb = new byte[i + 1];
-            System.arraycopy(decy, 0, nb, 0, nb.length);
-            return nb;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+    public static byte[] decrypt(byte[] result, Key key) throws Exception {
+        Cipher cipher = Cipher.getInstance(AES_ECB_Padding);
+        //初始化密码器，第一个参数为加密(Encrypt_mode)或者解密(Decrypt_mode)操作，第二个参数为使用的KEY
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(result);
+    }
+
+    /**
+     * CBC加密
+     *
+     * @param bytes 明文
+     * @param key   密码
+     * @param iv    偏移量16位
+     * @return 加密后
+     * @throws Exception Exception
+     */
+    public static byte[] encryptCBC(byte[] bytes, byte[] key, String iv) throws Exception {
+        SecretKey secretKey = new SecretKeySpec(paddingData(key), AES);
+        Cipher cipher = Cipher.getInstance(AES_CBC_Padding);
+        byte[] ivBytes = createIV(iv);
+        IvParameterSpec ipc = new IvParameterSpec(ivBytes);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, ipc);
+        return cipher.doFinal(bytes);
+    }
+
+    /**
+     * CBC解密
+     *
+     * @param bytes 密文
+     * @param key   密码
+     * @param iv    偏移量16位
+     * @return 解密后
+     * @throws Exception Exception
+     */
+    public static byte[] decryptCBC(byte[] bytes, byte[] key, String iv) throws Exception {
+        SecretKey secretKey = new SecretKeySpec(paddingData(key), AES);
+        Cipher cipher = Cipher.getInstance(AES_CBC_Padding);
+        byte[] ivBytes = createIV(iv);
+        IvParameterSpec ipc = new IvParameterSpec(ivBytes);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, ipc);
+        return cipher.doFinal(bytes);
+    }
+
+    /**
+     * ECB加密
+     *
+     * @param bytes 明文
+     * @param key   密码
+     * @return 加密后
+     * @throws Exception Exception
+     */
+    public static byte[] encryptECB(byte[] bytes, byte[] key) throws Exception {
+        SecretKey secretKey = new SecretKeySpec(paddingData(key), AES);
+        Cipher cipher = Cipher.getInstance(AES_ECB_Padding);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        return cipher.doFinal(bytes);
+    }
+
+    /**
+     * ECB解密
+     *
+     * @param bytes 密文
+     * @param key   密码
+     * @return 解密后
+     * @throws Exception Exception
+     */
+    public static byte[] decryptECB(byte[] bytes, byte[] key) throws Exception {
+        SecretKey secretKey = new SecretKeySpec(paddingData(key), AES);
+        Cipher cipher = Cipher.getInstance(AES_ECB_Padding);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        return cipher.doFinal(bytes);
     }
 
 
     // 加密成Base64字符串
-    public static String encryptToBase64(String data, String password) {
-        byte[] en = encrypt(data.getBytes(), password);
+    public static String encryptToBase64(String data, String password) throws Exception {
+        byte[] en = encryptECB(data.getBytes(), password.getBytes());
         return YBase64.encode(en);
     }
 
     // 解密Base64字符串
-    public static String decryptFromBase64(String base64, String password) {
+    public static String decryptFromBase64(String base64, String password) throws Exception {
         byte[] encryptedBytes = YBase64.decode(base64);
-        byte[] de = decrypt(encryptedBytes, password);
+        byte[] de = decryptECB(encryptedBytes, password.getBytes());
         return new String(de);
     }
 
     // 加密成16进制字符串
-    public static String encryptToHex(String data, String password) {
-        byte[] en = encrypt(data.getBytes(), password);
+    public static String encryptToHex(String data, String password) throws Exception {
+        byte[] en = encryptECB(data.getBytes(), password.getBytes());
         return YConvert.bytesToHexString(en);
     }
 
     // 解密16进制字符串
-    public static String decryptFromHex(String hexText, String password) {
+    public static String decryptFromHex(String hexText, String password) throws Exception {
         byte[] encryptedBytes = YConvert.hexStringToByte(hexText);
-        byte[] de = decrypt(encryptedBytes, password);
+        byte[] de = decryptECB(encryptedBytes, password.getBytes());
         return new String(de);
     }
 
