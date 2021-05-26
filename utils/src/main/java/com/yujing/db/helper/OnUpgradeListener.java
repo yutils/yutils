1c0b9f9ca8d3fbaa55bcb6c0b0632f01eda5fd72
余静 <yu1441@qq.com>
2021-05-26 11:42:40 +0800

    更新

---
+++ utils/src/main/java/com/yujing/db/helper/OnUpgradeListener.java
@@ -0,0 +1,11 @@
+package com.yujing.db.helper;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * 版本更新监听
+ * @author 余静 2021年5月26日10:33:10
+ */
+public interface OnUpgradeListener {
+    void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion);
+}
