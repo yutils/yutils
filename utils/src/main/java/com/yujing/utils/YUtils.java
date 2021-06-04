15f08c5da36a38210399f52b623a818c5a5ca9d4
余静 <yu1441@qq.com>
2021-06-04 14:22:31 +0800

    更

--- utils/src/main/java/com/yujing/utils/YUtils.java
+++ utils/src/main/java/com/yujing/utils/YUtils.java
@@ -251,11 +251,7 @@ public class YUtils {
      * @param isFullScreen 全屏否
      */
     public static void setFullScreen(Activity activity, boolean isFullScreen) {
-        if (isFullScreen) {
-            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏通知栏,通知栏透明:FLAG_FORCE_NOT_FULLSCREEN
-        } else {
-            activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//显示通知栏
-        }
+       YScreenUtil.setFullScreen(activity, isFullScreen);
     }
 
     /**
@@ -265,31 +261,7 @@ public class YUtils {
      * @param isFullScreen 沉浸式否
      */
     public static void setImmersive(Activity activity, boolean isFullScreen) {
-        if (isFullScreen) {
-            if (Build.VERSION.SDK_INT >= 19) {
-                Window window = activity.getWindow();
-                //沉浸式
-                View decorView = window.getDecorView();
-                decorView.setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-                //关闭输入法后，不显示虚拟按键
-                WindowManager.LayoutParams params = window.getAttributes();
-                params.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE;
-                window.setAttributes(params);
-            }
-        } else {
-            if (Build.VERSION.SDK_INT >= 19) {
-                View decorView = activity.getWindow().getDecorView();
-                decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-            }
-        }
+        YScreenUtil.setImmersive(activity, isFullScreen);
     }
 
     /**
